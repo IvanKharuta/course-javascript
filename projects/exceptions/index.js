a2ae617d0869457ead1dd921f387178ddda00d7f
ivan-kharuta <ivan-kharuta@users.noreply.github.com>
2021-02-01 13:07:41 +0300

    Homework 3

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -16,7 +16,23 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
-function isAllTrue(array, fn) {}
+function isAllTrue(array, fn) {
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array');
+  }
+
+  for (const value of array) {
+    if (!fn(value)) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 /*
  Задание 2:
@@ -34,7 +50,23 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array');
+  }
+
+  for (const value of array) {
+    if (fn(value)) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 /*
  Задание 3:
@@ -47,7 +79,23 @@ function isSomeTrue(array, fn) {}
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn, ...args) {}
+function returnBadArguments(fn, ...args) {
+  const newArray = [];
+
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  for (const value of args) {
+    try {
+      fn(value);
+    } catch {
+      newArray.push(value);
+    }
+  }
+
+  return newArray;
+}
 
 /*
  Задание 4:
@@ -66,7 +114,54 @@ function returnBadArguments(fn, ...args) {}
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator(number = 0) {}
+function calculator(number = 0) {
+  if (isNaN(number)) {
+    throw new Error('number is not a number');
+  }
+
+  return {
+    sum: function (...args) {
+      let result = number;
+
+      for (const el of args) {
+        result += el;
+      }
+
+      return result;
+    },
+    dif: function (...args) {
+      let result = number;
+
+      for (const el of args) {
+        result -= el;
+      }
+
+      return result;
+    },
+    div: function (...args) {
+      let result = number;
+
+      for (const el of args) {
+        if (el === 0) {
+          throw new Error('division by 0');
+        }
+
+        result /= el;
+      }
+
+      return result;
+    },
+    mul: function (...args) {
+      let result = number;
+
+      for (const el of args) {
+        result *= el;
+      }
+
+      return result;
+    },
+  };
+}
 
 /* При решении задач, постарайтесь использовать отладчик */
 
